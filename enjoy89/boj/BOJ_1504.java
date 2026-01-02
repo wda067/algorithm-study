5496a03e0a5d2ca0186ebc1b47a7b2aecaf41412
jeondui <jeondui@gmail.com>
2026-01-03 01:50:43 +0900

    refactor: [BOJ] 1504번

--- enjoy89/boj/BOJ_1504.java
+++ enjoy89/boj/BOJ_1504.java
@@ -1,13 +1,7 @@
 package boj;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class BOJ_1504 {
 
@@ -26,8 +20,8 @@ public class BOJ_1504 {
 		}
 	}
 
+	static List<Node>[] graph;
 	static int N;
-	static List<List<Node>> graph;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,9 +29,9 @@ public class BOJ_1504 {
 		N = Integer.parseInt(st.nextToken());
 		int E = Integer.parseInt(st.nextToken());
 
-		graph = new ArrayList<>();
-		for (int i = 0; i <= N; i++) {
-			graph.add(new ArrayList<>());
+		graph = new List[N + 1];
+		for (int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<>();
 		}
 
 		for (int i = 0; i < E; i++) {
@@ -45,49 +39,53 @@ public class BOJ_1504 {
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			graph.get(a).add(new Node(b, c));
-			graph.get(b).add(new Node(a, c));
+			// 양방향 그래프
+			graph[a].add(new Node(b, c));
+			graph[b].add(new Node(a, c));
 		}
 
 		st = new StringTokenizer(br.readLine());
-		// 반드시 거쳐야 하는 경로 2개
+		// 반드시 거쳐야 하는 경로 2가지
 		int v1 = Integer.parseInt(st.nextToken());
 		int v2 = Integer.parseInt(st.nextToken());
 
-		int[] dist = dijkstra(1);
-		int[] distV1 = dijkstra(v1);
-		int[] distV2 = dijkstra(v2);
+		int[] dist1 = dijkstra(1); // 1번에서 각 노드의 최단 거리
+		int[] distV1 = dijkstra(v1); // v1에서 각 노드의 최단 거리
+		int[] distV2 = dijkstra(v2); // v2에서 각 노드의 최단 거리
 
 		// 1 -> v1 -> v2 -> N
-		long r1 = (long)dist[v1] + distV1[v2] + distV2[N];
-		// 1 -> v2 -> v1 -> N
-		long r2 = (long)dist[v2] + distV2[v1] + distV1[N];
+		long route1 = (long)dist1[v1] + distV1[v2] + distV2[N];
 
-		long answer = Math.min(r1, r2);
+		// 1 -> v2 -> v1 -> N
+		long route2 = (long)dist1[v2] + distV2[v1] + distV1[N];
+
+		long answer = Math.min(route1, route2);
 		if (answer >= Integer.MAX_VALUE) {
 			answer = -1;
 		}
 		System.out.println(answer);
+
 	}
 
 	public static int[] dijkstra(int start) {
 		int[] dist = new int[N + 1];
 		Arrays.fill(dist, Integer.MAX_VALUE);
-
+		dist[start] = 0;
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		pq.offer(new Node(start, 0));
-		dist[start] = 0;
 
 		while (!pq.isEmpty()) {
 			Node cur = pq.poll();
-			for (Node next : graph.get(cur.vertex)) {
+
+			for (Node next : graph[cur.vertex]) {
 				int newCost = cur.cost + next.cost;
-				if (dist[next.vertex] > newCost) {
+				if (newCost < dist[next.vertex]) {
 					dist[next.vertex] = newCost;
 					pq.offer(new Node(next.vertex, newCost));
 				}
 			}
 		}
+
 		return dist;
 	}
 }
