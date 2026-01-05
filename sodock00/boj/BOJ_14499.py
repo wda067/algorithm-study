6c05f491ef5f3e3c240276ee9d7b927a932f2959
이은진 <12191804@inha.edu>
2026-01-06 00:05:31 +0900

    feat: [BOJ] 14499번
    시뮬레이션. 문제 조건 잘 보고 구현 필요
    주사위를 동·서·북·남으로 굴릴 때마다 윗면,아랫면,옆면 값이 서로 바뀌는 걸 정확히 관리
    이동하려는 위치가 지도 범위 안일 때만 굴리고, 칸의 값이 0이면 주사위 바닥 값을 복사

---
+++ sodock00/boj/BOJ_14499.py
@@ -0,0 +1,35 @@
+# 14499 주사위 굴리기
+import sys
+input = sys.stdin.readline
+
+# 주사위가 이동했을 때마다 상단에 쓰여있는 값을 구하기
+N, M, ci, cj, K = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+lst = list(map(int, input().split()))
+
+n1=n2=n3=n4=n5=n6=0
+di, dj = [0, 0, 0, -1, 1], [0, 1, -1, 0, 0] # 동서북남
+alst = []
+for dr in lst:
+    # [1] 이동 후 범위내이면 처리
+    ni, nj = ci+di[dr], cj+dj[dr]
+    if 0<=ni<N and 0<=nj<M:
+        # [2] 주사위 면 굴리기
+        if dr==1:       # 동
+            n1,n3,n4,n6 = n4,n1,n6,n3
+        elif dr==2:     # 서
+            n1,n3,n4,n6 = n3,n6,n1,n4
+        elif dr==3:     # 북
+            n1,n2,n5,n6 = n5,n1,n6,n2
+        else:           # 남
+            n1,n2,n5,n6 = n2,n6,n1,n5
+
+        # [3] 이동한 바닥판이 0인지 여부에 따라 처리
+        if arr[ni][nj]==0:
+            arr[ni][nj]=n6
+        else:
+            n6, arr[ni][nj]=arr[ni][nj],0
+
+        alst.append(n1) # 윗면의 값을 ans에 추가
+        ci, cj = ni,nj  # 이동처리
+print(*alst, sep='\n')
